5316a857ccb41a49b369c44fcbcfb304c8e9adfe
Mayank Verma <errmayank@gmail.com>
2024-03-15 00:02:54 +0530

    nest scroll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,10 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script lang="ts">
+	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import '../app.pcss';
+</script>
+
+<ScrollArea orientation="vertical" class="h-[calc(100dvh_-_56px_-_1px)] w-full">
+	<section class="app-container">
+		<slot />
+	</section>
+</ScrollArea>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div class="my-8" />
-<section class="mx-auto flex w-full max-w-3xl items-center justify-center px-0 sm:px-6">
+<section class="mx-auto flex w-full max-w-3xl flex-col items-center justify-center px-0 sm:px-6">
 	<div class="w-full bg-card">
 		<ScrollArea type="auto" orientation="horizontal" class="flex h-24">
 			<div class="flex size-full items-center justify-center">
@@ -17,5 +17,59 @@
 				<div class="h-7 w-[200px] bg-red-900" />
 			</div>
 		</ScrollArea>
+		<ScrollArea type="auto" orientation="vertical" class="flex h-96">
+			<div class="flex size-full items-center justify-center">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt maximus blandit.
+				Etiam pulvinar mauris vitae blandit porttitor. Cras sagittis suscipit lorem, sollicitudin
+				tempor risus fermentum nec. Sed ac leo nec massa posuere suscipit at sed dui. Donec ut
+				consectetur nibh, a dignissim arcu. Proin interdum ipsum eu vulputate mattis. Sed efficitur
+				velit eget pretium bibendum. Duis lectus nisi, sollicitudin in congue non, elementum sed
+				massa. Suspendisse eu maximus tellus, ut egestas sapien. Donec nec odio venenatis, gravida
+				nibh non, ultrices risus. Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci
+				luctus et ultrices posuere cubilia curae; In ultricies elit vitae tellus maximus
+				condimentum. Nunc id lobortis dolor, id lobortis sapien. Nam pellentesque tortor et enim
+				efficitur imperdiet. Sed sagittis, dui eget laoreet pretium, mauris nisl eleifend sapien, in
+				sagittis massa ipsum at libero. Ut vitae laoreet elit. Vivamus luctus nisl non justo
+				sodales, sit amet laoreet neque pretium. Donec elementum massa odio, non scelerisque diam
+				pretium in. In tincidunt turpis urna. Quisque congue tortor sit amet porta vestibulum.
+				Aenean suscipit ultricies justo, malesuada volutpat urna suscipit a. Maecenas vitae libero
+				felis. Sed metus metus, fermentum vel arcu in, scelerisque malesuada massa. Etiam
+				ullamcorper et sapien ac ornare. Ut lobortis leo euismod dolor volutpat, ut fringilla mi
+				rutrum. Praesent vitae diam velit. Cras urna nisl, auctor vel viverra vitae, elementum vitae
+				dolor. Etiam accumsan dignissim dolor. Nam cursus dui at lectus molestie, eget sagittis urna
+				consequat. Maecenas quis gravida sem. Sed posuere dui urna, quis convallis eros malesuada
+				id. Pellentesque eu eleifend augue, luctus tincidunt eros. Vivamus pulvinar massa in enim
+				congue, a suscipit dolor mattis. Sed interdum tellus scelerisque libero rhoncus facilisis.
+				Sed rutrum pulvinar libero et commodo. Sed posuere bibendum nunc, eu luctus nibh aliquam ut.
+				Duis aliquet lectus purus. Proin malesuada ac erat id laoreet. Aliquam elementum risus
+				consequat sollicitudin cursus.
+			</div>
+		</ScrollArea>
+	</div>
+	<div class="my-16" />
+	<div class="flex size-full items-center justify-center">
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt maximus blandit. Etiam
+		pulvinar mauris vitae blandit porttitor. Cras sagittis suscipit lorem, sollicitudin tempor risus
+		fermentum nec. Sed ac leo nec massa posuere suscipit at sed dui. Donec ut consectetur nibh, a
+		dignissim arcu. Proin interdum ipsum eu vulputate mattis. Sed efficitur velit eget pretium
+		bibendum. Duis lectus nisi, sollicitudin in congue non, elementum sed massa. Suspendisse eu
+		maximus tellus, ut egestas sapien. Donec nec odio venenatis, gravida nibh non, ultrices risus.
+		Nulla facilisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+		curae; In ultricies elit vitae tellus maximus condimentum. Nunc id lobortis dolor, id lobortis
+		sapien. Nam pellentesque tortor et enim efficitur imperdiet. Sed sagittis, dui eget laoreet
+		pretium, mauris nisl eleifend sapien, in sagittis massa ipsum at libero. Ut vitae laoreet elit.
+		Vivamus luctus nisl non justo sodales, sit amet laoreet neque pretium. Donec elementum massa
+		odio, non scelerisque diam pretium in. In tincidunt turpis urna. Quisque congue tortor sit amet
+		porta vestibulum. Aenean suscipit ultricies justo, malesuada volutpat urna suscipit a. Maecenas
+		vitae libero felis. Sed metus metus, fermentum vel arcu in, scelerisque malesuada massa. Etiam
+		ullamcorper et sapien ac ornare. Ut lobortis leo euismod dolor volutpat, ut fringilla mi rutrum.
+		Praesent vitae diam velit. Cras urna nisl, auctor vel viverra vitae, elementum vitae dolor.
+		Etiam accumsan dignissim dolor. Nam cursus dui at lectus molestie, eget sagittis urna consequat.
+		Maecenas quis gravida sem. Sed posuere dui urna, quis convallis eros malesuada id. Pellentesque
+		eu eleifend augue, luctus tincidunt eros. Vivamus pulvinar massa in enim congue, a suscipit
+		dolor mattis. Sed interdum tellus scelerisque libero rhoncus facilisis. Sed rutrum pulvinar
+		libero et commodo. Sed posuere bibendum nunc, eu luctus nibh aliquam ut. Duis aliquet lectus
+		purus. Proin malesuada ac erat id laoreet. Aliquam elementum risus consequat sollicitudin
+		cursus.
 	</div>
 </section>
